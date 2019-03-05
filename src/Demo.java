c1f0fe207894e405ea2777bd0df192b953f8933e
Sumeet Kaur <sumeet.smile@gmail.com>
2019-03-05 15:18:13 +1100

    commit to develop branch by India dev

--- src/Demo.java
+++ src/Demo.java
@@ -24,6 +24,7 @@ public class Demo {
 		//ch.close();//closes current browser
 		//ch.quit();//closes all browsers opened by selenium script
 		System.out.println("Uploaded by Developer in USA after changes");
+		System.out.println("Changes made in develop branch by Indian developer");
 	}
 
 }
